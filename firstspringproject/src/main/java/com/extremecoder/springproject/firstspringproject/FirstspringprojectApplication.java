8e465a8f3fab4aa2c1715fd3f7b19c5e119b6714
Rakib Hasan <rakib_hasan6@yahoo.com>
2018-04-04 15:21:44 +0600

    Added first project in spring boot

---
+++ firstspringproject/src/main/java/com/extremecoder/springproject/firstspringproject/FirstspringprojectApplication.java
@@ -0,0 +1,12 @@
+package com.extremecoder.springproject.firstspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspringprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstspringprojectApplication.class, args);
+	}
+}
